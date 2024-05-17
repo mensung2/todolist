775de2f232011ef3f950101619c1f4aa0885fd8f
minseo <mensung6@gmail.com>
2024-05-18 01:59:20 +0900

    update

--- components/todo/Todocontainer.jsx
+++ components/todo/Todocontainer.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Todoform from "./Todoform";
 import Todolist from "./Todolist";
 
 const Todocontainer = () => {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "할 일 1",
+      content: "할 일 내용1",
+      isDone: false,
+    }
+  ]);
+
   return (
     <div>
-      <Todoform />
+      <Todoform setTodos={setTodos}/>
       <div>
         <Todolist />
         <Todolist />
--- components/todo/Todoform.jsx
+++ components/todo/Todoform.jsx
@@ -1,14 +1,46 @@
+const Todoform = ({setTodos}) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-const Todoform = () => {
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const content = formData.get("content");
+
+    const nextTodo = {
+      id: crypto.randomUUID(),
+      title,
+      content,
+      isDone: false,
+    };
+
+    console.log("nextTodo :>> ",nextTodo);
+
+    setTodos((prevTodos) => [...prevTodos, nextTodo])
+    e.target.reset();
+    
+  };
+
   return (
     <div>
-    <form>
-        <input id="title" type="text" placeholder="제목을 입력하세요"  size="20" />
-        <input id="content" type="text" placeholder="내용을 입력하세요"  size="14" />
-        <button id="makepost" type="submit">등록</button>
-    </form>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="제목을 입력하세요"
+          size="20"
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="내용을 입력하세요"
+          size="14"
+          name="content"
+        />
+        <button id="makepost" type="submit">
+          등록
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Todoform
+export default Todoform;
